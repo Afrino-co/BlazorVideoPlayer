54e9bcae115ffa9f0c2fe169c2978154e08af128
Meysam Moghaddam <meysam.mogaddam@gmail.com>
2021-04-25 15:17:28 +0430

    Add OnEnded Event

--- wwwroot/main.js
+++ wwwroot/main.js
@@ -1,5 +1,5 @@
 ﻿
-export function load(settings, controls,elementId) {
+export function load(settings, controls, elementId, component) {
 
     var settingsArray = settings.split(",");
     var controlsArray = controls.split(",");
@@ -8,6 +8,9 @@ export function load(settings, controls,elementId) {
         controls: controlsArray,
 
         quality: { default: 576, options: [4320, 2880, 2160, 1440, 1080, 720, 576, 480, 360, 240, 200, 100, 50, 20] }
+    });
+    player.on('ended', event => {
+        component.invokeMethodAsync('onEnded');
     });
     //const players = Array.from(document.querySelectorAll('.js-player')).map(p => new Plyr(p, {
     //    settings: settingsArray,
